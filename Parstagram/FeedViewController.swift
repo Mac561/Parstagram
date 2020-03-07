f878e2eaccbb2412865fcf91ae47c25e178dd049
Mac561 <maccooper561@gmail.com>
2020-03-06 19:00:39 -0800

    new commit

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -61,4 +61,33 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let  post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comment")
+        comment["text"] = "This is a rendom comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+       post.saveInBackground { (success, error) in
+            if success{
+                print("Comment Saved")
+                   }else{
+                print("Error saving comment")
+        }
+    }
+    
+        func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+        }
+    
+    }
 }
---
+++ Parstagram/commentCell.swift
@@ -0,0 +1,24 @@
+//
+//  commentCell.swift
+//  Parstagram
+//
+//  Created by Mac Cooper on 3/6/20.
+//  Copyright © 2020 Mac Cooper. All rights reserved.
+//
+
+import UIKit
+
+class commentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
